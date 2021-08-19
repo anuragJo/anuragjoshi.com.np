2de8cd781cd6861aea903dd0da54f2708a6046d6
Anurag Joshi <ar_joshi@hotmail.com>
2021-08-19 22:30:53 +0545

    10:30

--- js/app.js
+++ js/app.js
@@ -2,24 +2,58 @@
 function contactSubmit(){
 	
 }
+
+	  var loged=document.getElementById("showInfo");
+		var form=document.getElementById("login");
+		var info=document.getElementById("logedIn");
+
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+
+		
+		form.style.display="none";
+		info.style.display="block";
+		loged.innerHTML="User Logged in :: User Id: "+user.uid+" User Email: "+user.email;
+    // ...
+  } else {
+	  form.style.display="block";
+	  info.style.display="none";
+  }
+});
+	
+	
+	
+	function logOut(){
+		
+		firebase.auth().signOut().then(() => {
+  		// Sign-out successful.
+		}).catch((error) => {
+			window.alert(error.code+" : "+ error.message)
+  // An error happened.
+});
+		
+	}
+
 function signIn(){
 	
 	var f_email=document.getElementById("email");
 	var f_password=document.getElementById("password");
 	let email=f_email.value;
 	let password=f_password.value;
-	console.log(email+" " +password);
+	
 	firebase.auth().signInWithEmailAndPassword(email, password)
   .then((userCredential) => {
     // Signed in
     var user = userCredential.user;
-		colsole.log("Success");
+		
     // ...
   })
   .catch((error) => {
     var errorCode = error.code;
     var errorMessage = error.message;
-		console.log(errorcode+" : "+errorMessage);
+		window.alert(errorCode + " : " + errorMessage);
   });
-	
 }
