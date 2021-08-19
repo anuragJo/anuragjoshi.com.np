e44d99c4fe3eb43b1fe3efb2e0ff99f9a3a4e135
Anurag Joshi <ar_joshi@hotmail.com>
2021-08-19 21:32:38 +0545

    9:32

--- js/app.js
+++ js/app.js
@@ -4,21 +4,22 @@ function contactSubmit(){
 }
 function signIn(){
 	
-	var email=document.getElementById("email")
-	var password=document.getElementById("password")
-	
-	
-	firebase.auth().signInWithEmailAndPassword(email.value, password.value)
+	var f_email=document.getElementById("email");
+	var f_password=document.getElementById("password");
+	let email=f_email.value;
+	let password=f_password.value;
+	console.log(email+" " +password);
+	firebase.auth().signInWithEmailAndPassword(email, password)
   .then((userCredential) => {
     // Signed in
     var user = userCredential.user;
-		colsole.log("Success")
+		colsole.log("Success");
     // ...
   })
   .catch((error) => {
     var errorCode = error.code;
     var errorMessage = error.message;
-		console.log(errorcode+" : "+errorMessage)
+		console.log(errorcode+" : "+errorMessage);
   });
 	
 }
